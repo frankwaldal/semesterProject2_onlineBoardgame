c1a403f527f9bcbd4298e9b1714affbd0c9da456
frankwaldal <frank.sondre.waldal@entur.org>
2020-04-28 14:36:45 +0200

    added char-select, basic game functionality and assets

--- site/scripts/game.js
+++ site/scripts/game.js
@@ -1,46 +1,267 @@
-var canvas = document.querySelector('#canvas');
+let tiles = [];
+let startX = 0;
+let startY = 0;
+let goalX = 0;
+let goalY = 0;
+
+let playerOne = {
+    position: 0
+}
+let playerTwo = {
+    position: 0
+}
+let charOne = JSON.parse(localStorage.getItem('p1'));
+let charTwo = JSON.parse(localStorage.getItem('p2'));
+playerOne = {...playerOne, ...charOne};
+playerTwo = {...playerTwo, ...charTwo};
+let battle = false;
+let turn = 0;
+let p1StartRoll = 0;
+let p2StartRoll = 0;
+
+const canvas = document.querySelector('#canvas');
 let idealWidth = 1280;
 let idealHeight = 850;
 let width = window.innerWidth - 20;
 let factor = width / idealWidth;
 let height = idealHeight * factor
-if (height > window.innerHeight) {
-    height = window.innerHeight - 20;
+if (height > (window.innerHeight - 90)) {
+    height = window.innerHeight - 90;
     factor = height / idealHeight;
     width = idealWidth * factor;
 }
+document.querySelector('#canvasBg').style.height = `${height}px`;
+document.querySelector('#canvasBg').style.width = `${width}px`;
 canvas.width = width;
 canvas.height = height;
-var ctx = canvas.getContext('2d');
+const ctx = canvas.getContext('2d');
 
-
-const bgImg = new Image();
-bgImg.src = 'assets/imgs/mountainPath.jpg';
-bgImg.onload = function() {
-    ctx.drawImage(bgImg, 0, 0, canvas.width, canvas.height);
-    drawTiles();
+function decideStarter() {
+    document.querySelector('#gameOverlay').style.display = 'block';
+    document.querySelector('#gameOverlay').innerHTML = '<button id="closeOverlay" onclick="closeOverlay();">Close</button><p>Roll for who to start.</p><p>Player 1 roll first.</p>';
+    document.querySelector('#playerOne').addEventListener('click', decideStarterTwo);
 }
 
-function drawTiles() {
-    const goalTile = new Image();
-    goalTile.src = 'assets/svg/goalTile.svg';
-    goalTile.onload = function() {
-        let x = canvas.width * 0.2;
-        let y = canvas.height * 0.2;
-        let tileWidth = canvas.width * 0.12;
-        let tileFactor = tileWidth / goalTile.width;
-        let tileHeight = goalTile.height * tileFactor;
-        ctx.drawImage(goalTile, x, y, tileWidth, tileHeight);
-    }
+function decideStarterTwo() {
+    document.querySelector('#playerOne').removeEventListener('click', decideStarterTwo);
+    let rolling = setInterval(playerOneRollingDice, 100);
+    setTimeout(function() {
+        clearInterval(rolling);
+        let roll = _.random(1,6);
+        let dices = ['assets/svg/p1OneDice.svg','assets/svg/p1TwoDice.svg','assets/svg/p1ThreeDice.svg','assets/svg/p1FourDice.svg','assets/svg/p1FiveDice.svg','assets/svg/p1SixDice.svg'];
+        document.querySelector('#p1DiceWrapper').innerHTML = `<img src="${dices[roll - 1]}" alt="Player 1 Dice" class="dice">`;
+        p1StartRoll = roll;
+        document.querySelector('#gameOverlay').style.display = 'block';
+        document.querySelector('#gameOverlay').innerHTML = '<button id="closeOverlay" onclick="closeOverlay();">Close</button><p>Roll for who to start.</p><p>Player 2 roll second.</p>';
+        document.querySelector('#playerTwo').addEventListener('click', decideStarterThree);
+    }, 2500);
+}
 
-    const startTile = new Image();
-    startTile.src = 'assets/svg/startTile.svg';
-    startTile.onload = function() {
-        let x = canvas.width * 0.6;
-        let tileWidth = canvas.width * 0.12;
-        let tileFactor = tileWidth / goalTile.width;
-        let tileHeight = goalTile.height * tileFactor;
-        let y = canvas.height - (tileHeight + 5);
-        ctx.drawImage(startTile, x, y, tileWidth, tileHeight);
+function decideStarterThree() {
+    document.querySelector('#playerTwo').removeEventListener('click', decideStarterThree);
+    let rolling = setInterval(playerTwoRollingDice, 100);
+    setTimeout(function() {
+        clearInterval(rolling);
+        let roll = _.random(1,6);
+        let dices = ['assets/svg/p2OneDice.svg','assets/svg/p2TwoDice.svg','assets/svg/p2ThreeDice.svg','assets/svg/p2FourDice.svg','assets/svg/p2FiveDice.svg','assets/svg/p2SixDice.svg'];
+        document.querySelector('#p2DiceWrapper').innerHTML = `<img src="${dices[roll - 1]}" alt="Player 2 Dice" class="dice">`;
+        p2StartRoll = roll;
+        if (p1StartRoll > p2StartRoll) {
+            turn = 1;
+            document.querySelector('#gameOverlay').style.display = 'block';
+            document.querySelector('#gameOverlay').innerHTML = '<button id="closeOverlay" onclick="closeOverlay();">Close</button><p>Player 1 starts.</p>';
+            initiateBoard(0, 0);
+        } else if (p2StartRoll > p1StartRoll) {
+            turn = 2;
+            document.querySelector('#gameOverlay').style.display = 'block';
+            document.querySelector('#gameOverlay').innerHTML = '<button id="closeOverlay" onclick="closeOverlay();">Close</button><p>Player 2 starts.</p>';
+            initiateBoard(0, 0);
+        } else if (p1StartRoll === p2StartRoll) {
+            document.querySelector('#gameOverlay').style.display = 'block';
+            document.querySelector('#gameOverlay').innerHTML = '<button id="closeOverlay" onclick="closeOverlay();">Close</button><p>You rolled the same, try again.</p><p>Player 1 roll first.</p>';
+            document.querySelector('#playerOne').addEventListener('click', decideStarterTwo);
+        }
+    }, 2500);
+}
+
+function initiateBoard(positionOne, positionTwo) {
+    fetch('assets/data/coords.json')
+        .then(resolve => {
+            resolve.json().then(data => {
+                let coords = data;
+                fetch('assets/data/tiles.json')
+                    .then(resolve => {
+                        resolve.json().then(data => {
+                            tiles = _.shuffle(data);
+                            goalX = canvas.width * 0.205;
+                            goalY = canvas.height * 0.23;
+                            startX = canvas.width * 0.6;
+                            startY = canvas.height - ((canvas.height * 0.12) + 5);
+                            for (let i=0;i < tiles.length;i++) {
+                                tiles[i].x = canvas.width * coords[i].x;
+                                tiles[i].y = canvas.height * coords[i].y;
+                            }
+                            playerOneTokenPlace(positionOne);
+                            playerTwoTokenPlace(positionTwo);
+                            document.querySelector('#playerOne').addEventListener('click', playerOneRoll);
+                            document.querySelector('#playerTwo').addEventListener('click', playerTwoRoll);
+                        })
+                    })
+                    .catch(err => {console.log(err)});
+            })
+        })
+        .catch(err => {console.log(err)});
+}
+
+function movePieces(positionOne, positionTwo) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    playerOneTokenPlace(positionOne);
+    playerTwoTokenPlace(positionTwo);
+}
+
+function playerOneTokenPlace(position) {
+    const playerOneToken = new Image();
+    playerOneToken.src = 'assets/svg/player1.svg';
+    playerOneToken.onload = function() {
+        let tokenWidth = canvas.width * 0.05;
+        let tokenFactor = tokenWidth / playerOneToken.width;
+        let tokenHeight = playerOneToken.height * tokenFactor;
+        if (position === 0) {
+            ctx.drawImage(playerOneToken, (startX + (tokenWidth / 2)), (startY + tokenHeight), tokenWidth, tokenHeight);
+        } else if (position > 30) {
+            ctx.drawImage(playerOneToken, (goalX + (tokenWidth / 2)), (goalY + tokenHeight), tokenWidth, tokenHeight);
+        } else {
+            let index = position - 1;
+            ctx.drawImage(playerOneToken, tiles[index].x, (tiles[index].y + (tokenHeight / 1.9)), tokenWidth, tokenHeight);
+        }
     }
+}
+
+function playerTwoTokenPlace(position) {
+    const playerTwoToken = new Image();
+    playerTwoToken.src = 'assets/svg/player2.svg';
+    playerTwoToken.onload = function() {
+        let tokenWidth = canvas.width * 0.05;
+        let tokenFactor = tokenWidth / playerTwoToken.width;
+        let tokenHeight = playerTwoToken.height * tokenFactor;
+        if (position === 0) {
+            ctx.drawImage(playerTwoToken, (startX + (tokenWidth)), (startY + tokenHeight), tokenWidth, tokenHeight);
+        } else if (position > 30) {
+            ctx.drawImage(playerTwoToken, (goalX + (tokenWidth)), (goalY + tokenHeight), tokenWidth, tokenHeight);
+        } else {
+            let index = position - 1;
+            ctx.drawImage(playerTwoToken, tiles[index].x, (tiles[index].y - (tokenHeight / 7)), tokenWidth, tokenHeight);
+        }
+    }
+}
+
+decideStarter();
+
+function playerOneRollingDice() {
+    let dices = ['assets/svg/p1OneDice.svg','assets/svg/p1TwoDice.svg','assets/svg/p1ThreeDice.svg','assets/svg/p1FourDice.svg','assets/svg/p1FiveDice.svg','assets/svg/p1SixDice.svg'];
+    dices = _.shuffle(dices);
+    document.querySelector('#p1DiceWrapper').innerHTML = `<img src="${dices[0]}" alt="Player 1 Dice" class="dice">`;
+}
+
+function playerTwoRollingDice() {
+    let dices = ['assets/svg/p2OneDice.svg','assets/svg/p2TwoDice.svg','assets/svg/p2ThreeDice.svg','assets/svg/p2FourDice.svg','assets/svg/p2FiveDice.svg','assets/svg/p2SixDice.svg'];
+    dices = _.shuffle(dices);
+    document.querySelector('#p2DiceWrapper').innerHTML = `<img src="${dices[0]}" alt="Player 2 Dice" class="dice">`;
+}
+
+function playerOneRoll() {
+    if (turn === 1) {
+        if (!battle) {
+            let rolling = setInterval(playerOneRollingDice, 100);
+            setTimeout(function() {
+                clearInterval(rolling);
+                let roll = _.random(1,6);
+                let dices = ['assets/svg/p1OneDice.svg','assets/svg/p1TwoDice.svg','assets/svg/p1ThreeDice.svg','assets/svg/p1FourDice.svg','assets/svg/p1FiveDice.svg','assets/svg/p1SixDice.svg'];
+                document.querySelector('#p1DiceWrapper').innerHTML = `<img src="${dices[roll - 1]}" alt="Player 1 Dice" class="dice">`;
+                for (let i=0;i < roll;i++) {
+                    let num = i+1;
+                    playerOne.position++;
+                    let pos = playerOne.position;
+                    if (i === (roll-1)) {
+                        setTimeout(() => {
+                            movePieces(pos, playerTwo.position);
+                            if (playerOne.position > 30) {
+
+                            } else {
+                                battleCheck();
+                            }
+                        }, 750*num);
+                    } else {
+                        setTimeout(() => {
+                            movePieces(pos, playerTwo.position);
+                        }, 750*num);
+                    }
+                }
+                function battleCheck() {
+                    if (tiles[playerOne.position - 1].battle) {
+                        console.log('battle');
+                    } else {
+                        if (roll !== 6) {
+                            turn = 2;
+                        }
+                    }
+                }
+            }, 2500);
+        } else {
+
+        }
+    } else {
+        alert('Player 2 turn!');
+    }
+}
+
+function playerTwoRoll() {
+    if (turn === 2) {
+        if (!battle) {
+            let rolling = setInterval(playerTwoRollingDice, 100);
+            setTimeout(function() {
+                clearInterval(rolling);
+                let roll = _.random(1,6);
+                let dices = ['assets/svg/p2OneDice.svg','assets/svg/p2TwoDice.svg','assets/svg/p2ThreeDice.svg','assets/svg/p2FourDice.svg','assets/svg/p2FiveDice.svg','assets/svg/p2SixDice.svg'];
+                document.querySelector('#p2DiceWrapper').innerHTML = `<img src="${dices[roll - 1]}" alt="Player 2 Dice" class="dice">`;
+                for (let i=0;i < roll;i++) {
+                    let num = i+1;
+                    playerTwo.position++;
+                    let pos = playerTwo.position;
+                    if (i === (roll-1)) {
+                        setTimeout(() => {
+                            movePieces(playerOne.position, pos);
+                            if (playerTwo.position > 30) {
+
+                            } else {
+                                battleCheck();
+                            }
+                        }, 750*num);
+                    } else {
+                        setTimeout(() => {
+                            movePieces(playerOne.position, pos);
+                        }, 750*num);
+                    }
+                }
+                function battleCheck() {
+                    if (tiles[playerTwo.position - 1].battle) {
+                        console.log('battle');
+                    } else {
+                        if (roll !== 6) {
+                            turn = 1;
+                        }
+                    }
+                }
+            }, 2500);
+        } else {
+
+        }
+    } else {
+        alert('Player 1 turn!');
+    }
+}
+
+function closeOverlay() {
+    document.querySelector('#gameOverlay').style.display = 'none';
+    document.querySelector('#gameOverlay').innerHTML = '';
 }
---
+++ site/scripts/setup.js
@@ -0,0 +1,85 @@
+let chars = [];
+
+function localPlay() {
+    fetch('assets/data/chars.json')
+        .then(resolve => {
+            resolve.json().then(data => {
+                chars = data;
+                let options = '';
+                for (let i=0;i < chars.length;i++) {
+                    options += `<option value="${i}">${chars[i].name}</option>`;
+                }
+                document.querySelector('main').innerHTML = `<div class="fullWidth">
+                        <p id="error"></p>
+                    </div>
+                    <div class="playSelection">
+                        <label for="charOne">Player 1 character: </label>
+                        <select id="charOne" onchange="charPreview(this);">
+                            ${options}
+                        </select>
+                        <label for="charOneNick">Player 1 nickname: </label>
+                        <input type="text" id="charOneNick">
+                        <div id="charOneSelected">
+                            <h2>${chars[0].name}</h2>
+                            <img src="${chars[0].imgUrl}" alt="${chars[0].name}">
+                            <p><b>Class:</b> ${chars[0].class}</p>
+                            <p><b>Weapon:</b> ${chars[0].weapon}</p>
+                            <p><b>Gender:</b> ${chars[0].gender}</p>
+                        </div>
+                    </div>
+                    <div class="playSelection">
+                        <label for="charTwo">Player 2 character: </label>
+                        <select id="charTwo" onchange="charPreview(this);">
+                            ${options}
+                        </select>
+                        <label for="charTwoNick">Player 2 nickname: </label>
+                        <input type="text" id="charTwoNick">
+                        <div id="charTwoSelected">
+                            <h2>${chars[0].name}</h2>
+                            <img src="${chars[0].imgUrl}" alt="${chars[0].name}">
+                            <p><b>Class:</b> ${chars[0].class}</p>
+                            <p><b>Weapon:</b> ${chars[0].weapon}</p>
+                            <p><b>Gender:</b> ${chars[0].gender}</p>
+                        </div>
+                    </div>
+                    <div class="fullWidth">
+                        <button onClick="location.reload();">Back</button>
+                        <button onClick="startGame();">Play</button>
+                    </div>`;
+                    window.scrollTo({top: document.body.scrollHeight, behavior: 'smooth'});
+            })
+        })
+        .catch(err => {console.log(err)});
+}
+
+function onlinePlay() {
+    document.querySelector('main').innerHTML = '<h2>We are sorry, but we do not support online play yet.</h2><button class="center" onClick="location.reload();">Back</button>';
+}
+
+function charPreview(e) {
+    document.querySelector(`#${e.id}Selected`).innerHTML = `<h2>${chars[e.value].name}</h2>
+        <img src="${chars[e.value].imgUrl}" alt="${chars[e.value].name}">
+        <p><b>Class:</b> ${chars[e.value].class}</p>
+        <p><b>Weapon:</b> ${chars[e.value].weapon}</p>
+        <p><b>Gender:</b> ${chars[e.value].gender}</p>`;
+}
+
+function startGame() {
+    let playerOne = document.querySelector('#charOne');
+    let playerTwo = document.querySelector('#charTwo');
+    if (playerOne.value === playerTwo.value) {
+        document.querySelector('#error').innerHTML = `Both players can't select the same character.`;
+        window.scrollTo({top: 0, behavior: 'smooth'});
+    } else {
+        let charOne = chars[playerOne.value];
+        let charTwo = chars[playerTwo.value];
+        charOne['nick'] = document.querySelector('#charOneNick').value;
+        charTwo['nick'] = document.querySelector('#charTwoNick').value;
+        localStorage.setItem('p1', JSON.stringify(charOne));
+        localStorage.setItem('p2', JSON.stringify(charTwo));
+        window.location.href = 'game.html';
+    }
+}
+
+document.querySelector('#localPlay').addEventListener('click', localPlay);
+document.querySelector('#onlinePlay').addEventListener('click', onlinePlay);
